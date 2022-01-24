87e12def94e5ad688d64723445b334a88d0a3bf9
joao <joao.lobom@gmail.com>
2022-01-24 22:58:40 +0000

    Last try

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,46 @@
-// mod file_parser;
-// use std::io;
-// use std::io::prelude::*;
-
-// fn main() {
-//     let file_parser = new();
-//     println!("{}", file_parser)
-// }
-// struct Parser {
-//     order: String,
-//     user: i32,
-//     symbol: String,
-//     price: i32,
-//     qty: i32,
-//     side: String,
-//     order_user_id: i32,
-// }
-
-// // struct FileReader{
-// //     path: String
-// // }
-
-// // impl FileReader{
-//     fn new() -> String{
-//         println!("OK: aaa !");
-//         let reader = io::stdin();
-//         let mut buffer: String = String::new();
-
-//         reader.read_line(&mut buffer)
-//             .ok()
-//             .expect("ERRMSG");
-//         // let mut rdr = csv::Reader::from_string(buffer);
-//         return buffer;
-
-//     }
-
-//     // fn (path: String) -> Self {
-//     //     Self{
-//     //        path
-//     //     }
-//     // }
-// // }
-
-// impl Parser {
-//     fn new(
-//         order: String,
-//         user: i32,
-//         symbol: String,
-//         price: i32,
-//         qty: i32,
-//         side: String,
-//         order_user_id: i32,
-//     ) -> Self {
-//         Self {
-//             order,
-//             user,
-//             symbol,
-//             price,
-//             qty,
-//             side,
-//             order_user_id,
-//         }
-//     }
-
-//     fn run(self) {
-//         println!("parsing... {}", self.order)
-//     }
-// }
 use std::fs::File;
 use std::io;
 use std::io::prelude::*;
 use std::path::Path;
 
 fn main() {
+    let mut new_transaction = new();
+}
+#[derive(Debug)]
+struct InputTransaction {
+    order: String,
+    user: i32,
+    symbol: String,
+    price: i32,
+    qty: i32,
+    side: String,
+    order_user_id: i32,
+}
+
+impl InputTransaction{
+    fn transaction(&self, data: String) {
+        fn parse_csv() {
+            let mut rdr = csv::Reader::from_string(data)
+                .has_headers(false)
+                .delimiter(b';')
+                .flexible(true);
+            for row in rdr.records() {
+                let row = row.unwrap();
+                let file = InputTransaction {
+                    order: row[0].parse().unwrap(),
+                    user: row[1].parse().unwrap(),
+                    symbol: row[2].parse().unwrap(),
+                    price: row[3].parse().unwrap(),
+                    qty: row[4].parse().unwrap(),
+                    side: row[5].parse().unwrap(),
+                    order_user_id: row[6].parse().unwrap(),
+                };  
+            }
+        }
+    }
+}
+
+fn new() {
     let mut input = String::new();
     println!("What is the csv");
     match io::stdin().read_line(&mut input) {
@@ -92,13 +61,22 @@ fn main() {
     }
 }
 
-fn parse_csv(data: String) {
-    let mut rdr = csv::Reader::from_string(data)
-        .has_headers(false)
-        .delimiter(b';')
-        .flexible(true);
-    for row in rdr.records() {
-        let row = row.unwrap();
-        println!("{:?}", row);
-    }
-}
+// fn build_transaction(
+//     order: String,
+//     user: i32,
+//     symbol: String,
+//     price: i32,
+//     qty: i32,
+//     side: String,
+//     order_user_id: i32,
+// ) -> Transaction {
+//     Transaction {
+//         order: order,
+//         user: user,
+//         symbol: symbol,
+//         price: price,
+//         qty: qty,
+//         side: side,
+//         order_user_id: order_user_id,
+//     }
+// }
